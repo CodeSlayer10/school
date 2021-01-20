8a83ab256d46e7ca8414c817e86f5bd20eb5d74b
slimergamer <37372477+slimergamer@users.noreply.github.com>
2021-01-20 19:30:39 +0200

    Update school.py

--- school.py
+++ school.py
@@ -41,7 +41,7 @@ def random_song_player(num_of_songs):
 def menu(num):
     if num == 1:
         ticket_requested_amount = int(input("Enter Number of tickets: "))
-        glasses3d_requested_amount = int(input("Enter Number of tickets: "))
+        glasses3d_requested_amount = int(input("Enter Number of 3D glasses: "))
         total_cost = Total_Price_Calculator(ticket_requested_amount, glasses3d_requested_amount)
         print("your total cost will be {}".format(total_cost))
     if num == 2:
