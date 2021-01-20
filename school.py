ecb670917078c7c38e8cb09f3aee9971d14a3c85
slimergamer <37372477+slimergamer@users.noreply.github.com>
2021-01-20 14:28:28 +0200

    Create school.py

---
+++ school.py
@@ -0,0 +1,57 @@
+import random
+
+Ticket_Price = 42
+Glasses3D_Price = 5
+# Taken from a top 20 songs of 2020 chart.
+song_list = ["BLINDING LIGHTS", "DANCE MONKEY", "ROSES", "BEFORE YOU GO", "HEAD & HEART", "DON'T START NOW", "ROCKSTAR", "SOMEONE YOU LOVED", "	OWN IT", "WATERMELON SUGAR", "SAVAGE LOVE", "THE BOX", "SAY SO", "LONELY", "BREAKING ME", "ADORE YOU", "RAIN ON ME", "ROVER", "PHYSICAL", "MOOD"]
+list2 = list()
+fibonacci_list = list()
+
+
+def Total_Price_Calculator(Ticket_Amount, Glasses3D_Amount):
+    return Ticket_Price*Ticket_Amount+Glasses3D_Price*Glasses3D_Amount
+
+
+def fibonacci_sequence_generator(n):
+    num1, num2 = 0, 1
+    count = 0
+    if n <= 0:
+        print("use a positive number...")
+        quit()
+    else:
+        while count < n:
+            fibonacci_list.append(num1)
+            num3 = num1 + num2
+            num1 = num2
+            num2 = num3
+            count += 1
+
+
+def random_song_player(num_of_songs):
+    if num_of_songs > 20:
+        print("there are only 20 songs")
+        quit()
+
+    for i in range(1, num_of_songs):
+        random_int = random.randint(0, 19)
+        list2.append(song_list[random_int])
+
+
+def menu(num):
+    if num == 1:
+        ticket_requested_amount = int(input("Enter Number of tickets: "))
+        glasses3d_requested_amount = int(input("Enter Number of tickets: "))
+        total_cost = Total_Price_Calculator(ticket_requested_amount, glasses3d_requested_amount)
+        print("your total cost will be {}".format(total_cost))
+    if num == 2:
+        n = int(input("enter fibonacci sequence length: "))
+        fibonacci_sequence_generator(n)
+        print(fibonacci_list)
+    if num == 3:
+        num_of_songs = int(input("Enter number of songs: "))
+        random_song_player(num_of_songs)
+        print(list2)
+
+
+menu_num = int(input("1) Total_Price_Calculator \n2) fibonacci_sequence_generator \n3) random_song_player \nEnter question number: "))
+menu(menu_num)
